f2201170f213535ce2ddde0b09da8d15edaef038
FVK4484 <aitfercam@alum.us.es>
2026-09-15 16:14:00 +0200

    Apartados b, c, d.

--- src/ejercicios/Ejercicio1.java
+++ src/ejercicios/Ejercicio1.java
@@ -1,6 +1,8 @@
 package ejercicios;
 
-public class Ejercicio1 {
+import java.util.stream.Stream;
+
+public record Ejercicio1(Integer a, Integer b, Integer c, String ac) {
 	
 	private static String cadena(Integer n) {
 		return n.toString();
@@ -23,10 +25,94 @@ public class Ejercicio1 {
 	
 	//	b) Proporcione una solución eficiente recursiva final.
 	public static String fRecursivaFinal(Integer a, Integer b, Integer c) {
-		String f = null;
+		return fRecursivaFinal(a, b, c, "");
+	}
+	
+	public static String fRecursivaFinal(Integer a, Integer b, Integer c, String ac) {
+		if (a < 3 || b < 3 || c < 3) {
+			return cadena(a + b + c) + "y" + ac;
+		} else if (a < 5 || (b < a && b < c) || c < 5) {
+			return cadena(a * b * c) + "z" + ac;
+		} else if (a % 2 == 0 && b % 2 == 0 && c % 2 == 0) {
+			return fRecursivaFinal(a - 1, b / 2, c - 3, "+" + ac);
+		} else {
+			return fRecursivaFinal(a - 4, b / 3, c - 4, "#" + ac);
+		}
+	}
+	
+	//	c) Solución en notación funcional
+	public static String fIterativaFuncional(Integer a, Integer b, Integer c) {
+		Ejercicio1 res = Stream.iterate(Ejercicio1.of(a, b, c, ""), 
+				t -> t.siguiente())
+				.filter(p -> p.esCasoBase())
+				.findFirst()
+				.get();
 		
-		
-		return f;
+		return solucionBase(res.a(), res.b(), res.c()) + res.ac();
+	}
+	
+	public static Ejercicio1 of(Integer a, Integer b, Integer c, String ac) {
+		return new Ejercicio1(a,b,c,ac);
+	}
+	
+	public Boolean esCasoBase() {
+		return Ejercicio1.esCasoBase(a, b, c);
+	}
+	
+	public static Boolean esCasoBase(Integer a, Integer b, Integer c) {
+		Boolean caso1 = a < 3 || b < 3 || c < 3;
+		Boolean caso2 = a < 5 || (b < a && b < c) || c < 5;
+		return caso1 || caso2;
+	}
+	
+	public static String solucionBase(Integer a, Integer b, Integer c) {
+		String r = "";
+		if (a < 3 || b < 3 || c < 3) {
+			r = (a + b + c) + "y";
+		} else if (a < 5 || (b < a && b < c) || c < 5) {
+			r = (a * b * c) + "z";
+		}
+		return r;
+	}
+	
+	public Ejercicio1 siguiente() {
+		Integer a1 = a(), b1 = b(), c1 = c();
+		String ac1 = ac();
+		if (a1 % 2 == 0 && b1 % 2 == 0 && c1 % 2 == 0) {
+			ac1 = String.format("%s", "+") + ac1;
+			a1 = a1 - 1;
+			b1 = b1 / 2;
+			c1 = c1 - 3;
+		} else {
+			ac1 = String.format("%s", "#") + ac1;
+			a1 = a1 - 4;
+			b1 = b1 / 3;
+			c1 = c1 - 4;
+		}
+		return Ejercicio1.of(a1, b1, c1, ac1);
+	}
+	
+	//	d) Solución iterativa con while
+	public static String fIterativaImperativa(Integer a, Integer b, Integer c) {
+		Integer a_w = a;
+		Integer b_w = b;
+		Integer c_w = c;
+		String acum = "";
+		while (!esCasoBase(a_w, b_w, c_w)) {
+			if (a_w % 2 == 0 && b_w % 2 == 0 && c_w % 2 == 0) {
+				acum = "+" + acum;
+				a_w = a_w - 1;
+				b_w = b_w / 2;
+				c_w = c_w - 3;
+			} else {
+				acum = "#" + acum;
+				a_w = a_w - 4;
+				b_w = b_w / 3;
+				c_w = c_w - 4;
+			}
+		}
+		acum = solucionBase(a_w, b_w, c_w) + acum;
+		return acum;
 	}
 	
 }
--- src/tests/TestEjercicio1.java
+++ src/tests/TestEjercicio1.java
@@ -6,7 +6,14 @@ public class TestEjercicio1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Recursiva No Final " + Ejercicio1.fRecursivaNoFinal(7, 9, 7));
+		System.out.println(" Recursiva No Final: " + 
+				Ejercicio1.fRecursivaNoFinal(7, 9, 7));
+		System.out.println(" Recursiva Final: " + 
+				Ejercicio1.fRecursivaFinal(7, 9, 7));
+		System.out.println(" Iterativa Funcional: " + 
+				Ejercicio1.fIterativaFuncional(7, 9, 7));
+		System.out.println(" Iterativa Funcional: " + 
+				Ejercicio1.fIterativaImperativa(7, 9, 7));
 
 	}
 
